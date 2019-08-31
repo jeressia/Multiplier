162451f6e3f42e4a526c6f65a9be7bbd2c889a31
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-08-31 14:13:30 -0700

    wip

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Multiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a list of numbers separated by commas.");
+            var numbers = Console.ReadLine();
+
+            Console.WriteLine($"Your numbers are {numbers}");
+
+            var splitNumbers = numbers.Split(",");
+
+            Console.WriteLine($"Your split numbers look like this: {splitNumbers}");
+        }
+    }
+}
